cfca63abff6580d4e9c211f15629c0f083edbfd0
jgjgj <31095408+jgjgj@users.noreply.github.com>
2017-08-17 17:32:38 +0530

    Update r.js

--- r.js
+++ r.js
@@ -64,8 +64,96 @@ var isAnswerGiven = false;
 		//return false; // avoid to execute the actual submit of the form.
 	};
 
+	function beforeAjaxCalling(callback) {
+	
+		$.Topic("CQ10_ON_ANSWER_SUBMITTING").publish();
+		
+		
+		
+		var isTimerEnable = $("#timer-" + settings.widgetKey).attr('isTimerEnable');
+		var isTimePerQue = $("#timer-" + settings.widgetKey).attr('isTimePerQue');
 
+		if((isTimerEnable == true || isTimerEnable == "true") && (isTimePerQue == true || isTimePerQue == "true")) {
+			$.Topic("TIMER_WIDGET_PAUSE").publish();
+		}
+		if ($.isFunction(settings.onAnswerSubmitting)) {
+
+			settings.onAnswerSubmitting.call(this);
+		}
+		
+		callback();
+	}
+	
+	
+	
+	function ajaxCalling(callback) {
+		try{
+			var $form = $("#" + settings.form),
+			data = $form.serializeArray(),
+			url = $form.attr("action");
+			
+			$.post(url, data, function(responseText) {
+				
+				
+				
+				var parseData = $.parseJSON(responseText);
 				console.log(parseData);
+				
+				
+			    billingStatus = parseData.billingStatus;
+			     
+			    submitStatus = parseData.submitStatus;
+			     
+			   /* 
+			parseData.answerStatus = "right" ;
+			 answerStatus = parseData.answerStatus;
+			 parseData.givenAnswer ="option1";
+			 parseData.rightAnswer ="option1";
+			 */
+				console.log(parseData);
+				$.Topic("CQ10_ON_RESPONSE").publish();
+			    if(billingStatus === "200"){
+			    	
+			    	doBillingStatusTrue(parseData,function(){
+			    		
+			    		if(submitStatus === "200"){
+			    			
+			    			doSubmitStatusTrue(parseData,function(){
+			    				
+			    				doAnswerStatus(parseData,answerStatus);
+			    				
+			    				if(parseData.badgeReceived != null && typeof parseData.badgeReceived  !== "undefined")
+			    					showPopup(parseData.badgeReceived);
+			    				
+			    				if(parseData.skipQuestionPage == true) {
+			    					var newurl = $('#skipQuestionPageUrlDiv').attr('srcref');
+			    					setTimeout(function(){ window.location.href = newurl; }, 1000);
+			    					
+			    					//doAjaxToRefreshWidget(widgetKey);
+			    				} 
+			    				
+			    			});
+			        		
+			        	} else if(submitStatus === "5001") {
+			        		$.Topic("CQ10_ON_SUBMIT_STATUS_LIMIT_EXCEED").publish(parseData.submitMessage,settings.widgetKey);
+			        	} else {
+			        		$.Topic("CQ10_ON_SUBMIT_STATUS_FALSE").publish(parseData.submitMessage,settings.widgetKey);
+			        	}
+			    	});
+			    	
+			    	
+			    }else{
+			    	$.Topic("CQ10_ON_BILLING_FALSE").publish(parseData.billingMessage);
+			    }
+			    callback();
+			});
+		}catch(e){
+			
+		}
+		
+	}
+	
+	
 })(jQuery);
 
 function doRefreshWidgetForTimePerQuestion(widgetKey){
